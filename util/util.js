175626d83280e0fb618185f8937949b69decbd1f
Chris Ro <chris.ro@kareo.com>
2020-04-10 22:19:31 -0700

    added AWS sdk to util

--- util/util.js
+++ util/util.js
@@ -1,4 +1,11 @@
 const ErrorResponse = require('../util/ErrorResponse')
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION || 'us-west-2'
+})
 
 const phoneNumberRegEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 
