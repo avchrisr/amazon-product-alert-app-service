034658b89adeea7082a0eb6556627887a49faed5
Chris Ro <chris.ro@kareo.com>
2020-04-10 18:00:33 -0700

    added dotenv to data-loader

--- data-loader.js
+++ data-loader.js
@@ -6,6 +6,9 @@ const Bluebird = require('bluebird')
 const sampleData = require('./_data/items').items
 const AWS = require('aws-sdk')
 
+// load env variables
+require('dotenv').config({ path: './config/.env' });
+
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
